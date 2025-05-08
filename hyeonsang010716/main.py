71046db21ca91b022b651f23c17c68198550f742
hyeonsang010716 <kyr76789@gmail.com>
2025-05-08 16:45:26 +0900

    [FEAT] BOJ 17430 PS

--- hyeonsang010716/main.py
+++ hyeonsang010716/main.py
@@ -1,10 +1,36 @@
 def solve(inputs):
     answers = []
     
-    for input in inputs:
-        ans = 0
-        for i in range(1, int(input)+1):
-            ans += i
-        answers.append(str(ans))
-        
+    for Input in inputs:
+        l = Input.split("\n")
+        idx = 1
+        answer = ""
+        for _ in range(int(l[0])):
+            n = int(l[idx])
+            idx += 1
+            check = dict()
+
+            for i in range(n):
+                a , b = map(int,l[idx].split())
+                idx += 1
+                if a not in check:
+                    check[a] = {b}
+                else:
+                    check[a].add(b)
+
+            x = True
+
+            y = -1
+
+            for a in check.values():
+                if y == -1:
+                    y = a
+                else:
+                    if y != a:
+                        x = False
+                        break
+                    
+            if x: answer += "BALANCED" + "\n"
+            else: answer += "NOT BALANCED" + "\n"
+        answers.append(answer)
     return answers
