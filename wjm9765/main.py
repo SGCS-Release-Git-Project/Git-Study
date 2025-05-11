1c3646ff1bbceb53e9d02ddd94a2b3782ff6de25
wjm <wjm9765@naver.com>
2025-05-11 23:11:53 +0900

    [FIX] main.py 오류 수정

--- wjm9765/main.py
+++ wjm9765/main.py
@@ -1,9 +1,12 @@
 def solve(inputs):
-    T = int(inputs[0])
+    # 첫 번째 문자열을 줄 단위로 나눈다
+    lines = inputs[0].splitlines()
+    
+    T = int(lines[0])
     answers = []
-
+    
     for i in range(1, T + 1):
-        a, b = map(int, inputs[i].split())
+        a, b = map(int, lines[i].split())
         answers.append(a + b)
-
+    
     return answers
